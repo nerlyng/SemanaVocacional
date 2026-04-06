337f876188a27392623335c959c2209685737336
Nerlyng Quiroz <nerlynginformatica@gmail.com>
2026-04-06 11:04:18 -0600

    inital commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   output: "server",
+   adapter: vercel({
+    // aquí puedes poner opciones si quieres,
+    // por ejemplo isr: true
+  }),
+});
